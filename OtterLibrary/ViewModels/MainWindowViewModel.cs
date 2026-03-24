fd0d47718ad3c912536ad2596f557f51cb377d0b
Adela Pastekova <adpas25@student.sdu.dk>
2026-03-24 22:54:20 +0100

    mvvm

--- OtterLibrary/ViewModels/MainWindowViewModel.cs
+++ OtterLibrary/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,68 @@
-﻿namespace OtterLibrary.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Security.Cryptography;
+
+namespace OtterLibrary.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private string usernameInput;
+
+    [ObservableProperty]
+    private string passwordInput;
+
+    [ObservableProperty]
+    private string loginResult;
+
+    private readonly string _username = "admin";
+    private readonly byte[] _savedSalt;
+    private readonly byte[] _savedHash;
+
+    public IRelayCommand SignInCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        (_savedHash, _savedSalt) = HashPassword("1234");
+        SignInCommand = new RelayCommand(SignIn);
+    }
+
+    private void SignIn()
+    {
+        if (UsernameInput != _username)
+        {
+            LoginResult = "Wrong username";
+            return;
+        }
+
+        bool isValid = VerifyPassword(PasswordInput, _savedHash, _savedSalt);
+
+        LoginResult = isValid ? "Login SUCCESS" : "Wrong username or password";
+    }
+
+    private (byte[] hash, byte[] salt) HashPassword(string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(16);
+
+        var hash = new Rfc2898DeriveBytes(
+            password,
+            salt,
+            10000,
+            HashAlgorithmName.SHA256
+        ).GetBytes(32);
+
+        return (hash, salt);
+    }
+
+    private bool VerifyPassword(string password, byte[] hash, byte[] salt)
+    {
+        var hashToCompare = new Rfc2898DeriveBytes(
+            password,
+            salt,
+            10000,
+            HashAlgorithmName.SHA256
+        ).GetBytes(32);
+
+        return CryptographicOperations.FixedTimeEquals(hash, hashToCompare);
+    }
 }
--- OtterLibrary/Views/MainWindow.axaml.cs
+++ OtterLibrary/Views/MainWindow.axaml.cs
@@ -3,64 +3,13 @@ using Avalonia.Interactivity;
 using System;
 using System.Security.Cryptography;
 namespace OtterLibrary.Views;
+using OtterLibrary.ViewModels;
 
 public partial class MainWindow : Window
 {
-    private readonly string _username = "admin";
-
-    private readonly byte[] _savedSalt;
-    private readonly byte[] _savedHash;
-
     public MainWindow()
     {
         InitializeComponent();
-        (_savedHash, _savedSalt) = HashPassword("1234"); 
-    }
-
-    public void SignInButton_Click(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("Attempting login...");
-        var inputUsername = UsernameTextBox.Text;
-        var inputPassword = PasswordTextBox.Text;
-
-        if (inputUsername != _username)
-        {
-            Console.WriteLine("Wrong username");
-            return;
-        }
-
-        bool isValid = VerifyPassword(inputPassword, _savedHash, _savedSalt);
-
-        if (isValid)
-            Console.WriteLine("Login SUCCESS");
-        else
-            Console.WriteLine("Wrong password");
-    }
-
-    
-    private (byte[] hash, byte[] salt) HashPassword(string password)
-    {
-        byte[] salt = RandomNumberGenerator.GetBytes(16);
-
-        var hash = new Rfc2898DeriveBytes(
-            password,
-            salt,
-            10000,
-            HashAlgorithmName.SHA256
-        ).GetBytes(32);
-
-        return (hash, salt);
-    }
-
-    private bool VerifyPassword(string password, byte[] hash, byte[] salt)
-    {
-        var hashToCompare = new Rfc2898DeriveBytes(
-            password,
-            salt,
-            10000,
-            HashAlgorithmName.SHA256
-        ).GetBytes(32);
-
-        return CryptographicOperations.FixedTimeEquals(hash, hashToCompare);
+        DataContext = new MainWindowViewModel();
     }
 }
