c11ddb577168e10847299c828404077082cf298f
Zolizoli110 <zolika.suveges@gmail.com>
2026-04-08 12:13:28 +0200

    Models

---
+++ FlightTracker/Models/Airport.cs
@@ -0,0 +1,22 @@
+﻿namespace FlightTracker.Models;
+
+public class Airport
+{
+    public string IataCode { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    
+    public Airport(string iataCode, string name, string city, string country, double latitude, double longitude)
+    {
+        IataCode = iataCode;
+        Name = name;
+        City = city;
+        Country = country;
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+}
---
+++ FlightTracker/Models/Flight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FlightTracker.Models;
+
+public class Flight
+{
+    public int FlightNumber { get; set; }
+    public string AirlineName { get; set; }
+    public string AirlineCode { get; set; }
+    public Airport DepartureAirport { get; set; }
+    public Airport ArrivalAirport { get; set; }
+    public DateTime ScheduledDepartureTime { get; set; }
+    public DateTime ScheduledArrivalTime { get; set; }
+    public string AircraftType { get; set; }
+    public string status { get; set; }
+    
+    public Flight(int flightNumber, string airlineName, string airlineCode, Airport departureAirport, Airport arrivalAirport, DateTime scheduledDepartureTime, DateTime scheduledArrivalTime, string aircraftType, string status)
+    {
+        FlightNumber = flightNumber;
+        AirlineName = airlineName;
+        AirlineCode = airlineCode;
+        DepartureAirport = departureAirport;
+        ArrivalAirport = arrivalAirport;
+        ScheduledDepartureTime = scheduledDepartureTime;
+        ScheduledArrivalTime = scheduledArrivalTime;
+        AircraftType = aircraftType;
+        this.status = status;
+    }
+}
